19f3d29236d9a8ccd68677bdb772b7171cadf734
JuliaBeeHappy <109751706+JuliaBeeHappy@users.noreply.github.com>
2022-07-23 21:40:54 -0300

    test

--- src/main/java/lesson1/HomeWork1A.java
+++ src/main/java/lesson1/HomeWork1A.java
@@ -4,5 +4,6 @@ public class HomeWork1A {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Julia Starovska");
+        System.out.println("Julia Starovska");
     }
 }
